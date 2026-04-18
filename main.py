5acbf33e1bb1a966c7e5c275dd16733ae5184dd9
rhassettcoding <rianhassett.2023@gmail.com>
2026-04-17 22:00:00 -0700

    Created a select all button, removed scan button and made app automatically scan upon selecting a folder.

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import sys
 import os
 from PySide6.QtWidgets import (
     QApplication, QWidget, QVBoxLayout, QLabel,
-    QPushButton, QListWidget, QFileDialog, QMessageBox
+    QPushButton, QListWidget, QFileDialog, QMessageBox,QAbstractItemView
 )
 
 
 class FileCleanerApp(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Student File Cleaner")
+        self.setWindowTitle("OtterDelete")
         self.setGeometry(100, 100, 600, 400)
 
         self.selected_folder = ""
@@ -23,12 +23,16 @@ class FileCleanerApp(QWidget):
         self.select_button.clicked.connect(self.select_folder)
         layout.addWidget(self.select_button)
 
-        self.scan_button = QPushButton("Scan Files")
-        self.scan_button.clicked.connect(self.scan_files)
-        layout.addWidget(self.scan_button)
-
+        # self.scan_button = QPushButton("Scan Files")
+        # self.scan_button.clicked.connect(self.scan_files)
+        # layout.addWidget(self.scan_button)
         self.file_list = QListWidget()
+        self.file_list.setSelectionMode(QAbstractItemView.MultiSelection) #allows us to select multiple files
         layout.addWidget(self.file_list)
+
+        self.select_all_button = QPushButton("Select All Files")
+        self.select_all_button.clicked.connect(self.file_list.selectAll)
+        layout.addWidget(self.select_all_button)
         
 
         self.delete_button = QPushButton("Delete Selected Files")
@@ -38,13 +42,15 @@ class FileCleanerApp(QWidget):
         self.setLayout(layout)
 
     def select_folder(self):
-        folder = QFileDialog.getExistingDirectory(self, "Select Folder")
+        folder = QFileDialog.getExistingDirectory(self, "Select Folder To Scan")
         if folder:
             # if there is pre-selected items remove them
             if(self.file_list):
                 self.file_list.clear()
             self.selected_folder = folder
             self.label.setText(f"Selected Folder: {folder}")
+            #scan the files directly after selecting a folder
+            self.scan_files() 
 
 
     def scan_files(self):
@@ -59,6 +65,7 @@ class FileCleanerApp(QWidget):
 
             if os.path.isfile(full_path):
                 self.file_list.addItem(full_path)
+
 
     def delete_files(self):
         selected_items = self.file_list.selectedItems()
@@ -76,7 +83,7 @@ class FileCleanerApp(QWidget):
 
         if reply == QMessageBox.Yes:
             for item in selected_items:
-                # replace with os.remove(file_path) when actually ready to remove
+                # replace with os.remove(file_path) when actually ready to delete files
                 self.file_list.takeItem(self.file_list.row(item))
 
             QMessageBox.information(self, "Done", "Selected files removed from the list.")
